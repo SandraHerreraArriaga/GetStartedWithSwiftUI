38792431352e4bf19a9aad012c60f1aa67062975
Sandra Herrera <sandra.herrera@edisoneffect.tech>
2019-06-25 09:44:55 -0500

    Vstack

---
+++ Vstack.swift
@@ -0,0 +1,27 @@
+//
+//  Vstack.swift
+//  SwiftByExample
+//
+//  Created by Sandra Herrera on 25/06/19.
+//  Copyright © 2019 Edison Effect. All rights reserved.
+//
+
+import SwiftUI
+
+struct Vstack : View {
+    var body: some View {
+        VStack {
+            Text("Elemento de Vstack")
+            Text("Elemento de Vstack")
+            Text("Elemento de Vstack")
+        }
+    }
+}
+
+#if DEBUG
+struct Vstack_Previews : PreviewProvider {
+    static var previews: some View {
+        Vstack()
+    }
+}
+#endif
