0e8e2da782e945a6b1a3f382a77e01b02a4a0d6b
Sandra Herrera <sandra.herrera@edisoneffect.tech>
2019-06-25 10:27:56 -0500

    List with Identifiable property, in this example I called UserIdent instead User because I have already this class for a example in last step .

---
+++ SwiftByExample/UserIdentifiable.swift
@@ -0,0 +1,38 @@
+//
+//  UserIdentifiable.swift
+//  SwiftByExample
+//
+//  Created by Sandra Herrera on 25/06/19.
+//  Copyright © 2019 Edison Effect. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserIdent: Identifiable {
+    var id : Int
+    var firstName : String
+    var lastName : String
+}
+
+
+struct UserIdentifiable : View {
+    var user : UserIdent
+    
+    var body: some View
+    {
+        Text("\(user.id) \(user.firstName) \(user.lastName)")
+    }
+}
+
+#if DEBUG
+struct UserIdentifiable_Previews : PreviewProvider {
+    static var previews: some View {
+        let user1 = UserIdent(id: 1, firstName: "Piper", lastName: "Chapman")
+        let user2 = UserIdent(id: 2, firstName: "Gloria", lastName: "Mendoza")
+        let users = [user1, user2]
+        
+        
+        return List(users, rowContent: UserIdentifiable.init)
+    }
+}
+#endif
